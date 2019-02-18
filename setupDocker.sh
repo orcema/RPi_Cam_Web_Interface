cb9517d64092f67171da61bc01d3230e8af9d548
Orce MARINKOVSKI <orce00@gmail.com>
2019-02-18 20:38:21 +0100

    added script for docker build image and setup container

---
+++ setupDocker.sh
@@ -0,0 +1,25 @@
+DIRECTORY="/opt/config"
+DOCKERFILE="/tmp/Dockerfile"
+
+# create the config folder in  /opt
+if [[ ! -e "$DIRECTORY" ]]; then
+        sudo mkdir -p /opt/config
+else
+        echo "[INFO]    folder "."$DIRECTORY"." exists !";
+fi
+
+# remove dockerfile if exist in tmp folder
+if [[ -f "$DOCKERFILE" ]]; then
+        sudo rm "$DOCKERFILE"
+fi
+
+cd /tmp
+
+# get docker file
+wget -q https://raw.githubusercontent.com/orcema/RPi_Cam_Web_Interface/master/Dockerfile
+
+# build docker image
+sudo docker build -t img_rpi_cam_web_om .
+
+#setup docker container
+sudo docker run -d --name=rpi_cam_web_om -p=81:80/tcp --volume=/opt/vc:/opt/vc --device=/dev/vchiq --device=/dev/vcsm img_rpi_cam_web_om
